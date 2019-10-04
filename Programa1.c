7a2581c6c8590e3a3b19c3b90f293b0666011f31
AngelLCJ <54157013+AngelLCJ@users.noreply.github.com>
2019-10-03 23:54:45 -0500

    Tarea
    Por Correa Juarez Angel Leonardo y Abrego Abascal Diego.

---
+++ Programa1.c
@@ -0,0 +1,22 @@
+﻿# include<stdio.h>
+int main (void) {
+int n,i,y,x,j,z;
+	scanf("%d",&n);
+        y=n-1;
+        x=0;
+        for(i=n;i>=1;i-=1){
+           for(j=1;j<=n;j++){
+              printf("%d",j);
+                }
+z=x*2;                                    while(z>0){
+	printf(" ");
+    z-=1;}
+    for(j=n;j>=1;j--){
+       printf("%d",j);
+                }             
+        n-=1;
+        x++;
+        printf("\n");
+}
+        return 0;
+}
---
+++ Programa3.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>                     
+int main(){                          
+printf("            1            \n");
+printf("          2 3 2          \n");
+printf("        3 4 5 4 3        \n");
+printf("      4 5 6 7 6 5 4      \n");
+printf("    5 6 7 8 9 8 7 6 5    \n"); 
+printf("  6 7 8 9 0 1 0 9 8 7 6  \n");
+printf("7 8 9 0 1 2 3 2 1 0 9 8 7\n");
+return 0;
+}
